0e2308aafea1f255c21238c97711e2fb3aaf965d
NguyenDuy <ichduy1882001@gmail.com>
2022-10-09 13:46:55 +0700

    fix get current user route

--- routes/user/user.js
+++ routes/user/user.js
@@ -4,7 +4,7 @@ const { userController } = require("../../controller/user");
 const { requireAuth } = require("../../middleware/authMiddleware");
 
 router.get("/all", requireAuth, userController.showAll);
+router.get("/current_user", userController.getCurrentUser);
 router.get("/:id", userController.show);
-router.get("/currentUser", userController.getCurrentUser);
 
 module.exports = router;

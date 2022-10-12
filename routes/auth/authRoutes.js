6d87c81111dd03c4b93c35d78a9d0bc3ed5dc73a
NguyenDuy <ichduy1882001@gmail.com>
2022-10-12 17:18:25 +0700

    fix sign up route

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { authController } = require("../../controller/auth");
+const upload = require("../../middleware/multer");
 
 router.post("/signup", upload.single("avatar"), authController.signup_post);
 router.post("/login", authController.login_post);

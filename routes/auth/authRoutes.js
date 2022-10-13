66650588e552c5472867253692a16f2166ec5df5
NguyenDuy <ichduy1882001@gmail.com>
2022-10-13 16:27:42 +0700

    add cloudinary

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { authController } = require("../../controller/auth");
 const upload = require("../../middleware/multer");
+const signCloud = require("../../middleware/signCloudinary");
 
-router.post("/signup", upload.single("avatar"), authController.signup_post);
+router.post("/signup", authController.signup_post);
 router.post("/login", authController.login_post);
+router.get("/sign_cloudinary", signCloud);
 router.get("/logout", authController.logout);
 
 module.exports = router;

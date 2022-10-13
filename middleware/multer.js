d176544a9fd581a32a15760c025c52ede6f19cb9
NguyenDuy <ichduy1882001@gmail.com>
2022-10-13 13:54:28 +0700

    change dir name

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,8 +1,11 @@
 const multer = require("multer");
+require("dotenv").config();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/uploads/");
+    const dirName = process.env.DEV ? "./uploads" : "/tmp/";
+    console.log(dirName);
+    cb(null, dirName);
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}${file.originalname}`);

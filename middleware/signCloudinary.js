b54e99af7a9d1091b445a7d0eb1357334e8a078f
NguyenDuy <ichduy1882001@gmail.com>
2022-10-13 17:24:20 +0700

    check env

--- middleware/signCloudinary.js
+++ middleware/signCloudinary.js
@@ -7,11 +7,15 @@ const signCloud = async (req, res) => {
   // Get the timestamp in seconds
   const timestamp = Math.round(new Date().getTime() / 1000);
 
+  console.log(process.env.CLOUDINARY_SECRET);
+
+  const params = {
+    timestamp: timestamp,
+  };
+
   // Get the signature using the Node.js SDK method api_sign_request
   const signature = await cloudinary.utils.api_sign_request(
-    {
-      timestamp: timestamp,
-    },
+    params,
     process.env.CLOUDINARY_SECRET
   );
 

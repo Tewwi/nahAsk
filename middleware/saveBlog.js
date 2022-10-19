746c99044c2cede8cf27a5e8b719bf8a27f4eed4
NguyenDuy <ichduy1882001@gmail.com>
2022-10-19 16:37:13 +0700

    fix upload img

--- middleware/saveBlog.js
+++ middleware/saveBlog.js
@@ -4,7 +4,7 @@ function saveBlog(path) {
     Object.keys(req.body).forEach((key) => {
       blog[key] = req.body[key];
     });
-    blog.thumb = req.thumb;
+    blog.thumb = req.body.thumb;
     blog.author = res.currUser;
     try {
       blog = await blog.save();
